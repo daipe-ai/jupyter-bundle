3b217f5c09b9e48807f95969c06e0ea89561f640
Lukáš Langr <lukas.langr@datasentics.com>
2021-10-27 13:24:57 +0200

    Add PandasDataFrameDisplay

--- src/jupyterbundle/JupyterBundle.py
+++ src/jupyterbundle/JupyterBundle.py
@@ -7,5 +7,6 @@ class JupyterBundle(Bundle):
     def modify_parameters(self, parameters: Box) -> Box:
         if is_jupyter_server_running():
             parameters.pysparkbundle.dataframe.show_method = "jupyter_display"
+            parameters.daipecore.pandas.dataframe.show_method = "jupyter_display"
 
         return parameters
