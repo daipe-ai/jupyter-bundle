3cbe0ca0ae8cafa600bb413fa6cef51ca8b33866
Lukáš Langr <lukas.langr@datasentics.com>
2021-10-27 13:24:08 +0200

    Make jupyter-bundle responsible for only displaying dataframes in a Jupyter environment

--- src/jupyterbundle/JupyterBundle.py
+++ src/jupyterbundle/JupyterBundle.py
@@ -1,9 +1,11 @@
 from box import Box
+from jupyterbundle.server_detector import is_jupyter_server_running
 from pyfonybundles.Bundle import Bundle
 
 
 class JupyterBundle(Bundle):
     def modify_parameters(self, parameters: Box) -> Box:
-        parameters.pysparkbundle.dataframe.show_method = "jupyter_display"
+        if is_jupyter_server_running():
+            parameters.pysparkbundle.dataframe.show_method = "jupyter_display"
 
         return parameters
